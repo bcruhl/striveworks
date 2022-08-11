ab6fec4b5354c52847cbe9482e24f2efe87c6661
Brian Ruhl <bcruhl@gmail.com>
2022-08-11 14:35:27 -0500

    Cleaning up express

--- server/index.js
+++ server/index.js
@@ -15,16 +15,7 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(cors(corsOptions)) 
 
-const superFakeCDNContent = [
-  {src: '/app/assets/file_example_GIF_500kB.gif'},
-  {src: '/app/assets/raccoon.jpg'},
-  {src: '/app/assets/food.jpg'},
-  {src: '/app/assets/reef.jpg'},
-  {src: '/app/assets/Small_Butterfly.jpg'},
-  {src: '/app/assets/Butter.jpeg'},
-  {src: '/app/assets/reef2.webp'},
-  {src: '/app/assets/dog_on_beach.png'},
-];
+const ASSET_PATH = '/app/assets/';
 
 app.get('/api/listImages', (req, res) => {
   res.send(getUploadedFiles());
@@ -32,33 +23,30 @@ app.get('/api/listImages', (req, res) => {
 
 const getUploadedFiles = () => {
   let filesOnSystem = [];
-  fs.readdirSync('./../app/assets').forEach(file => {
-    console.log("files:", file);
+  fs.readdirSync('./..' + ASSET_PATH).forEach(file => {
     filesOnSystem.push({src: '/app/assets/'+file});
   });
-  console.log("files on system:", filesOnSystem);
   return filesOnSystem;
 }
 
 app.get('/api/queryImageNames', (req, res) => {
   const query = req.query?.query ?? '';
-  let filteredNames = [];
   let filesOnSystem = getUploadedFiles();
+  const pathRegEx = new RegExp(ASSET_PATH, 'g');
   if (query !== "") {
-    filteredNames = filesOnSystem.filter((name) => {
-      return name.src.toLowerCase().includes(query.toLowerCase());
+    filesOnSystem = filesOnSystem.filter((name) => {
+      name = name.src.replace(pathRegEx, '').split('.')[0].toLowerCase();
+      return name.includes(query.toLowerCase());
     });
-  } else {
-    filteredNames = filesOnSystem;
   }
-  res.send(filteredNames);
+  res.send(filesOnSystem);
 });
 
 app.post('/api/uploadImage', (req, res) => {
   //WRITE FILE TO CDN
   const busboy = busboyCons({ headers: req.headers });
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    const saveTo = __dirname + '/../app/assets/' + filename.filename;
+    const saveTo = __dirname + '/..' + ASSET_PATH + filename.filename;
     console.log('Uploading: ' + saveTo);
     file.pipe(fs.createWriteStream(saveTo));
   });
@@ -69,17 +57,6 @@ app.post('/api/uploadImage', (req, res) => {
   });
   return req.pipe(busboy);
 });
-
-const saveFile = (url, body) => {
-  fs.writeFile(url, body, (err) => {
-    if (err) {
-      console.log('Error saving');
-      throw err;
-    } else {
-      console.log('It\'s saved!');
-    }
-  });
-}
 
 app.listen(3000, () =>
   console.log("API running on http://localhost:3000")
